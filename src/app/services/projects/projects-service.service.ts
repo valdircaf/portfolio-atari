f0f6950578fb4dab55a52bf93fda17012991f83c
valdircaf <valdircaf@hotmail.com>
2024-05-19 00:33:07 -0300

    att 2

--- src/app/services/projects/projects-service.service.ts
+++ src/app/services/projects/projects-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { Project } from 'src/app/interface/Project';
 import { HttpClient } from '@angular/common/http';
 
